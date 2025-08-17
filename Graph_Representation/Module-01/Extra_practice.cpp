5d35bcf409688d1fda1a70db57f4744c17b1f8dc
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-17 10:28:05 +0600

    added

---
+++ Graph_Representation/Module-01/Extra_practice.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N, E; // N for Node and E for Edge
+    cin >> N >> E;
+    int adj_mat[N][N];
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            adj_mat[i][j] = 0;
+        }
+    }
+
+    return 0;
+}
