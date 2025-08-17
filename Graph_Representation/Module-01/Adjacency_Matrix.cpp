3482a5e0691d7a7596c6b2b9839f7496eb4205f9
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-17 10:06:59 +0600

    added

--- Graph_Representation/Module-01/Adjacency_Matrix.cpp
+++ Graph_Representation/Module-01/Adjacency_Matrix.cpp
@@ -2,6 +2,31 @@
 using namespace std;
 int main()
 {
-    
+    int N, E; // N for Node and E for Edge
+    cin >> N >> E;
+    int adj_mat[N][N];
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            adj_mat[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << adj_mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    while (E--)
+    {
+        int a, b;
+        cin >> a >> b;
+    }
+
     return 0;
 }
