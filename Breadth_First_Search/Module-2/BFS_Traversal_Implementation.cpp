6e506b9b3464ee93915b3fd1e23ac9f7f26c20e2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-18 19:31:51 +0600

    added

--- Breadth_First_Search/Module-2/BFS_Traversal_Implementation.cpp
+++ Breadth_First_Search/Module-2/BFS_Traversal_Implementation.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
-    
+    int N, E;
+    cin >> N >> E;
+    vector<int> adj_list[N]; // array of vector
+
+    while (E--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a); // for undirected
+    }
     return 0;
 }
