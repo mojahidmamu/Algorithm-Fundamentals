acb3da8fdbeed0d23cb77c0c052ed2467374206b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-18 06:45:50 +0600

    added

--- Breadth_First_Search/Module-2/Adjacency_List.cpp
+++ Breadth_First_Search/Module-2/Adjacency_List.cpp
@@ -4,10 +4,23 @@ int main()
 {
     int N, E;
     cin >> N >> E;
+    vector<int> adj_list[N]; // array of vector
     while (E--)
     {
         int a, b;
         cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a); // for undirected
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << i << " -> ";
+        for (int x : adj_list[i])
+        {
+            cout << x << " ";
+        }
+        cout << endl;
     }
 
     return 0;
