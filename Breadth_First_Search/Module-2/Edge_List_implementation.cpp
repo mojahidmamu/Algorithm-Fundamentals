932cdfbdebe946b13228d34f79bf46e54bc8f916
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-18 06:52:23 +0600

    added

---
+++ Breadth_First_Search/Module-2/Edge_List_implementation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N, E;
+    cin >> N >> E;
+    vector<int> adj_list[N]; // array of vector
+    while (E--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a); // for undirected
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << i << " -> ";
+        for (int x : adj_list[i])
+        {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
