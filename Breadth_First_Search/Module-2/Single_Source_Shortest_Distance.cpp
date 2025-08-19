799f92f0d5e6e6c0ff80b9ae2c2a948d9ad3101b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-19 07:53:31 +0600

    added

--- Breadth_First_Search/Module-2/Single_Source_Shortest_Distance.cpp
+++ Breadth_First_Search/Module-2/Single_Source_Shortest_Distance.cpp
@@ -49,10 +49,11 @@ int main()
     cin >> source_code >> dst;
     bfs(source_code);
 
-    for (int i = 0; i < N; i++)
-    {
-        cout << i << " -> " << level[i] << endl;
-    }
+    // for (int i = 0; i < N; i++)
+    // {
+    //     cout << i << " -> " << level[i] << endl;
+    // }
 
+    cout << level[dst] << endl;
     return 0;
 }
