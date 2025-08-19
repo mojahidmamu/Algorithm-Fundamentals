71830b3ef3f9e07e94c28ce961ab1418209ff816
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-19 06:59:20 +0600

    added

---
+++ Breadth_First_Search/Module-2/Single_Source_Shortest_Distance.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> adj_list[1005]; //  global vector:
+bool vis[1005];
+
+void bfs(int source_code)
+{
+    queue<int> q;
+    q.push(source_code);
+    vis[source_code] = true;
+
+    while (!q.empty())
+    {
+        int parent = q.front();
+        q.pop();
+
+        cout << parent << " ";
+
+        for (int child : adj_list[parent])
+        {
+            if (vis[child] == false)
+            {
+                q.push(child);
+                vis[child] = true;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int N, E;
+    cin >> N >> E;
+
+    while (E--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a); // for undirected
+    }
+    memset(vis, false, sizeof(vis));
+    int source_code, dst;
+    cin >> source_code >> dst;
+    bfs(source_code);
+
+    if (vis[dst])
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
