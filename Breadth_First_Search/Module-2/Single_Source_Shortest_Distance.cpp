08279d02a6309a539cfc30d9b7ecd941217b3f36
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-19 07:51:51 +0600

    added

--- Breadth_First_Search/Module-2/Single_Source_Shortest_Distance.cpp
+++ Breadth_First_Search/Module-2/Single_Source_Shortest_Distance.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 vector<int> adj_list[1005]; //  global vector:
 bool vis[1005];
+int level[1005];
 
 void bfs(int source_code)
 {
     queue<int> q;
     q.push(source_code);
     vis[source_code] = true;
+    level[source_code] = 0;
 
     while (!q.empty())
     {
         int parent = q.front();
         q.pop();
 
-        cout << parent << " ";
+        // cout << parent << " ";
 
         for (int child : adj_list[parent])
         {
@@ -22,6 +24,7 @@ void bfs(int source_code)
             {
                 q.push(child);
                 vis[child] = true;
+                level[child] = level[parent] + 1;
             }
         }
     }
@@ -41,17 +44,14 @@ int main()
         adj_list[b].push_back(a); // for undirected
     }
     memset(vis, false, sizeof(vis));
+    memset(level, -1, sizeof(level));
     int source_code, dst;
     cin >> source_code >> dst;
     bfs(source_code);
 
-    if (vis[dst])
+    for (int i = 0; i < N; i++)
     {
-        cout << "Yes" << endl;
-    }
-    else
-    {
-        cout << "NO" << endl;
+        cout << i << " -> " << level[i] << endl;
     }
 
     return 0;
