5c2e76082814b1c1f48e6b1840b3649a2a091949
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-19 06:48:36 +0600

    added

--- Breadth_First_Search/Module-2/check_if_a_node_visited_or_not.cpp
+++ Breadth_First_Search/Module-2/check_if_a_node_visited_or_not.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> adj_lst[1005];
+bool vis[1005];
+
 int main()
 {
-    
+    int N, E;
+    cin >> N >> E;
+
+    while (E--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a); // for undirected
+    }
+    memset(vis, false, sizeof(vis));
+    bfs(0);
     return 0;
 }
