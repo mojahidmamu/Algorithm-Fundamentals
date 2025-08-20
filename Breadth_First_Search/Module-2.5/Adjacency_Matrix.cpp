b40492438afbae19ef5227d5cc331b52d6e972ee
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-20 10:17:48 +0600

    added

--- Breadth_First_Search/Module-2.5/Adjacency_Matrix.cpp
+++ Breadth_First_Search/Module-2.5/Adjacency_Matrix.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int adj_list[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            adj_list[i][j] = 0;
+        }
+    }
+
+    // for diagonal:
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                adj_list[i][j] = 1;
+            }
+        }
+    }
+
     return 0;
 }
