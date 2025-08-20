734a57b4cc2ce8d56b4384211e8680a2f5891de4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-20 19:50:40 +0600

    added

--- Breadth_First_Search/Module-2.5/Message_Route.cpp
+++ Breadth_First_Search/Module-2.5/Message_Route.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
-    
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> adj_list(n + 1);
+    while (m--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
     return 0;
 }
