b67c04a0af9150dc575b8c973b99358233b4bc7a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-20 16:22:53 +0600

    added

--- Depth_First_Search/Module-3/DFS_on_2D_Grid_Implementation.cpp
+++ Depth_First_Search/Module-3/DFS_on_2D_Grid_Implementation.cpp
@@ -1,7 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
+// Global array:
+int arr[20][20];
+// visited array:
+bool vis[20][20];
+
+vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+// Global integer:
+int n, m;
+
+bool valid(int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int dfs(int Si, int Sj)
+{
+}
+
 int main()
 {
-    
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][i];
+        }
+    }
+
+    int Si, Sj;
+    cin >> Si >> Sj;
+
+    memset(vis, false, sizeof(vis));
+    dfs(Si, Sj);
     return 0;
 }
