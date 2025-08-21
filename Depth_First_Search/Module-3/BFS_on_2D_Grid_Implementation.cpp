6a519269ae15a4e814c7275541c764e2988b0989
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-21 15:12:55 +0600

    added

--- Depth_First_Search/Module-3/BFS_on_2D_Grid_Implementation.cpp
+++ Depth_First_Search/Module-3/BFS_on_2D_Grid_Implementation.cpp
@@ -1,7 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
+// Global array:
+char arr[20][20];
+// visited array:
+bool vis[20][20];
+
+vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+// Global integer:
+int n, m;
+
+bool valid(int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+void dfs(int Si, int Sj)
+{
+    cout << Si << " " << Sj << endl;
+    vis[Si][Sj] = true;
+    for (int i = 0; i < 4; i++)
+    {
+        int ci = Si + d[i].first;
+        int cj = Sj + d[i].second;
+        if (valid(ci, cj) && vis[ci][cj] == false)
+        {
+            dfs(ci, cj);
+        }
+    }
+}
+
 int main()
 {
-    
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    int Si, Sj;
+    cin >> Si >> Sj;
+
+    memset(vis, false, sizeof(vis));
+    dfs(Si, Sj);
     return 0;
 }
