76ede0a858b1312383bc7ac9ca6bd136aa45721d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-21 16:16:24 +0600

    added

--- Depth_First_Search/Module-3.5/Problem_01.cpp
+++ Depth_First_Search/Module-3.5/Problem_01.cpp
@@ -5,7 +5,26 @@ bool vis[1005];
 
 int bfs(int start_node)
 {
-    
+    int count = 0;
+    queue<int> q;
+    q.push(start_node);
+    vis[start_node] = true;
+
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+
+        for (int child : adj_list(par))
+        {
+            if (!vis[child])
+            {
+                vis[child] = true;
+                q.push(child);
+            }
+        }
+    }
+    return count;
 }
 
 int main()
@@ -19,6 +38,8 @@ int main()
         adj_list[a].push_back(b);
         adj_list[b].push_back(a);
     }
+
+    memset(vis, false, sizeof(vis));
 
     int start_node;
     cin >> start_node;
