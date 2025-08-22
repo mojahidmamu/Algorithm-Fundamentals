ed4c08742c5212141177bf39e4e774ca75438f9e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-22 07:42:17 +0600

    added

--- Assignment-1/Module-4/Connected_or_Not.cpp
+++ Assignment-1/Module-4/Connected_or_Not.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 int main()
 {
+    int N, E;
+    cin >> N >> E;
+    while (E--)
+    {
+        int a, b;
+        cin >> a >> b;
+    }
+
+    // intput query: 
+    int Q;
+    cin >> Q;
+    while (Q--)
+    {
+        int A, B;
+        cin >> A >> B;
+    }
     
+
     return 0;
 }
