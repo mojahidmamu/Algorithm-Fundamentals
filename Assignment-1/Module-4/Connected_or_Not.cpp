abc357b9056604ecc57968940caad9ca82881fc3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-22 08:53:43 +0600

    added

--- Assignment-1/Module-4/Connected_or_Not.cpp
+++ Assignment-1/Module-4/Connected_or_Not.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> adj_list[1005];
 int main()
 {
     int N, E;
@@ -8,17 +9,26 @@ int main()
     {
         int a, b;
         cin >> a >> b;
+        adj_list[a].push_back(b);
     }
 
-    // intput query: 
+    // intput query:
     int Q;
     cin >> Q;
     while (Q--)
     {
         int A, B;
         cin >> A >> B;
+
+        if (adj_list[A].size(adj_list()))
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
     }
-    
 
     return 0;
 }
