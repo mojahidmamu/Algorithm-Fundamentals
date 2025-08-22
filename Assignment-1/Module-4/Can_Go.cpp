7eadccb1b2007733f4c86ed697eef656898f606e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-22 09:59:41 +0600

    added

--- Assignment-1/Module-4/Can_Go.cpp
+++ Assignment-1/Module-4/Can_Go.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
+char arr[1005][1005];
 using namespace std;
 int main()
 {
-    
+    int N, M;
+    cin >> N >> M;
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
     return 0;
 }
