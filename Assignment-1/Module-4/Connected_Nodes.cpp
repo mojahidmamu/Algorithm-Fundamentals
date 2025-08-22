9bd3a2a195833358458e149917ebd45529190cf3
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-22 09:02:53 +0600

    added

--- Assignment-1/Module-4/Connected_Nodes.cpp
+++ Assignment-1/Module-4/Connected_Nodes.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> adj_list[1005];
 int main()
 {
-    
+    int N, E;
+    cin >> N >> E;
+    while (E--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+
+    // intput query:
+    int Q;
+    cin >> Q;
+    while (Q--)
+    {
+    }
     return 0;
 }
