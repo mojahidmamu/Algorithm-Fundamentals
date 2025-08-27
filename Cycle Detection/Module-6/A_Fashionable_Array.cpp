fe0fd21f131e2dfcb40ecf0373fccc283edfd091
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-27 10:06:52 +0600

    added

--- Cycle Detection/Module-6/A_Fashionable_Array.cpp
+++ Cycle Detection/Module-6/A_Fashionable_Array.cpp
@@ -13,8 +13,31 @@ int main()
         {
             cin >> arr[i];
         }
-        
+
+        sort(arr, arr + N);
+
+        for (int i = 0; i < N; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+        int mn = INT_MAX;
+        int mx = INT_MIN;
+        for (int i = 0; i < N; i++)
+        {
+            if (arr[i] < arr[mn])
+            {
+                mn = arr[i];
+                break;
+            }
+            if (arr[mx] < arr[i])
+            {
+                mx = arr[i];
+                break;
+            }
+        }
     }
-    
+
     return 0;
 }
