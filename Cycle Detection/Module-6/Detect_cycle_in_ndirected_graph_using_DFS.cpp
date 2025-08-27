421e02f0c33839b67bfeed96f65e3d4f1bdaaae0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-27 08:57:44 +0600

    added

--- Cycle Detection/Module-6/Detect_cycle_in_ndirected_graph_using_DFS.cpp
+++ Cycle Detection/Module-6/Detect_cycle_in_ndirected_graph_using_DFS.cpp
@@ -5,28 +5,19 @@ vector<int> adj_list[1005];
 int parent[1005];
 bool isCycle;
 
-void bfs(int source)
+void dfs(int source)
 {
-    queue<int> q;
-    q.push(source);
     vis[source] = true;
-    while (!q.empty())
+    for (int child : adj_list[source])
     {
-        int par = q.front();
-        q.pop();
-        // cout << par << " ";
-        for (int child : adj_list[par])
+        if (vis[child] == true && parent[source] != child)
         {
-            if(vis[child] == true && parent[par] != child)
-            {
-                isCycle = true;
-            }
-            if (!vis[child])
-            {
-                q.push(child);
-                vis[child] = true;
-                parent[child] = par;
-            }
+            isCycle = true;
+        }
+        if (!vis[child])
+        {
+            dfs(child);
+            parent[child] = source;
         }
     }
 }
@@ -51,10 +42,9 @@ int main()
     {
         if (!vis[i])
         {
-            bfs(i);
+            dfs(i);
         }
     }
-
 
     // print condition:
     if (isCycle == true)
