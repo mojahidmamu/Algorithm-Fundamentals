92fb8090bbdcfb764a88621891c7fece5f7204ee
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-28 08:37:22 +0600

    added

--- Cycle Detection/Module-6/A_Dr_TC.cpp
+++ Cycle Detection/Module-6/A_Dr_TC.cpp
@@ -2,6 +2,38 @@
 using namespace std;
 int main()
 {
-    
+    int TestCase;
+    cin >> TestCase;
+    while (TestCase--)
+    {
+        int N;
+        cin >> N;
+        string s;
+        cin >> s;
+
+        int count = 0;
+        for (char ch : s)
+        {
+            if (ch == '1')
+            {
+                count++;
+            }
+        }
+
+        int ans = 0;
+        for (char ch : s)
+        {
+            if (ch == '1')
+            {
+                ans += count - 1;
+            }
+            else
+            {
+                ans += count + 1;
+            }
+        }
+        cout << ans << endl;
+    }
+
     return 0;
 }
