8b43a210e44026341f5a4ee547301a32b699ad3d
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-29 10:51:27 +0600

    added

--- Mid Term Exam/Module-8/Maze.cpp
+++ Mid Term Exam/Module-8/Maze.cpp
@@ -2,15 +2,48 @@
 using namespace std;
 
 int N, M;
-vector<string> grid;
+vector<string> maze;
+bool vis[1005][1005];
+int startX, startY;
+
+bool dfs(int x, int y)
+{
+}
 
 int main()
 {
     cin >> N >> M;
-    grid.resize(N);
+    maze.resize(N);
     for (int i = 0; i < N; i++)
     {
-        cin >> grid[i];
+        cin >> maze[i];
+        for (int j = 0; j < M; j++)
+        {
+            if (maze[i][j] == 'R')
+            {
+                startX = i;
+                startY = j;
+            }
+        }
     }
+    memset(vis, false, sizeof(vis));
+
+    bool found = dfs(startX, startY);
+
+    if (found)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            cout << maze[i] << endl;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < N; i++)
+        {
+            cout << maze[i] << endl;
+        }
+    }
+
     return 0;
 }
