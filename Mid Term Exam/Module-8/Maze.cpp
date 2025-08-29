c284cd7aff7086bf1663f6f4d31051f95a10e429
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-29 11:17:01 +0600

    added

--- Mid Term Exam/Module-8/Maze.cpp
+++ Mid Term Exam/Module-8/Maze.cpp
@@ -5,6 +5,8 @@ int N, M;
 vector<string> maze;
 bool vis[1005][1005];
 int startX, startY;
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {1, -1, 0, 0};
 
 bool valid(int x, int y)
 {
@@ -13,6 +15,30 @@ bool valid(int x, int y)
 
 bool dfs(int x, int y)
 {
+    if (maze[x][y] == 'D')
+    {
+        return true;
+    }
+    vis[x][y] = true;
+    for (int i = 0; i < 4; i++)
+    {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if (valid(nx, ny))
+        {
+            if (dfs(nx, ny))
+            {
+                if (maze[x][y] != 'R')
+                {
+                    maze[x][y] = 'X';
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
 }
 
 int main()
@@ -47,7 +73,7 @@ int main()
         for (int i = 0; i < N; i++)
         {
             cout << maze[i] << endl;
-        } 
+        }
     }
 
     return 0;
