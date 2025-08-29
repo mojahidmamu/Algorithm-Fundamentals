e608eef8b3135a2516779c8a9e1b6774ad3c3d27
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-29 06:51:09 +0600

    added

--- Mid Term Exam/Module-8/Same_Component.cpp
+++ Mid Term Exam/Module-8/Same_Component.cpp
@@ -4,12 +4,45 @@ using namespace std;
 int N, M;
 vector<string> grid(1005);
 bool vis[1005][1005];
+vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+
+bool valid(int x, int y)
+{
+    return (x >= 0 && x < N && y >= 0 && y < M && !vis[x][y] && grid[x][y] == '.');
+}
 
 bool bfs(int Si, int Sj, int Di, int Dj)
 {
     queue<pair<int, int>> q;
     q.push({Si, Sj});
     vis[Si][Sj] = true;
+
+    while (!q.empty())
+    {
+        auto par = q.front();
+        q.pop();
+        int x = par.first;
+        int y = par.second;
+
+        if (x == Di && y == Dj)
+        {
+            return true;
+        }
+
+        for (auto child : d)
+        {
+            int A = child.first;
+            int B = child.second;
+
+            int Nx = x + A;
+            int Ny = y + B;
+            if (valid(Nx, Ny))
+            {
+                vis[Nx][Ny] = true;
+                q.push({Nx, Ny});
+            }
+        }
+    }
 }
 
 int main()
@@ -30,7 +63,6 @@ int main()
         cout << "NO" << endl;
     }
     memset(vis, false, sizeof(vis));
-
 
     if (bfs(Si, Sj, Di, Dj))
     {
