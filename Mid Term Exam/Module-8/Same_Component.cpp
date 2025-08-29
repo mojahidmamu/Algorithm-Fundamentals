9d33874602227a1e17b52291652123e8eb061202
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-29 06:28:18 +0600

    added

--- Mid Term Exam/Module-8/Same_Component.cpp
+++ Mid Term Exam/Module-8/Same_Component.cpp
@@ -4,11 +4,21 @@ int main()
 {
     int N, M;
     cin >> N >> M;
+    vector<string> grid(N);
 
+    for (int i = 0; i < N; i++)
+    {
+         cin >> grid[i];
+    } 
     int Si, Sj;
     cin >> Si >> Sj;
     int Di, Dj;
     cin >> Di >> Dj;
-    
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << grid[i] << endl;
+    }
+
     return 0;
 }
