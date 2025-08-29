73c0c4dbf1f08926c8a14531515cec030322a022
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-29 10:43:19 +0600

    added

--- Mid Term Exam/Module-8/Maze.cpp
+++ Mid Term Exam/Module-8/Maze.cpp
@@ -1,7 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int N, M;
+vector<string> grid;
+
 int main()
 {
-    
+    cin >> N >> M;
+    grid.resize(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> grid[i];
+    }
     return 0;
 }
