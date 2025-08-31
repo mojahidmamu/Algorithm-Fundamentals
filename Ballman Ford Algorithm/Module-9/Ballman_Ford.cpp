240c15e31b79a5a7b25db01062f78b79858c58be
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-08-31 22:01:28 +0600

    added

--- Ballman Ford Algorithm/Module-9/Ballman_Ford.cpp
+++ Ballman Ford Algorithm/Module-9/Ballman_Ford.cpp
@@ -1,14 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
+
+class Edge
+{
+public:
+    int a, b, c;
+    Edge(int a, int b, int c)
+    {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+    }
+};
+
+int
+main()
 {
     int n, e;
     cin >> n >> e;
+    vector<Edge> edge_list;
     while (e--)
     {
         int a, b, c;
         cin >> a >> b >> c;
     }
-    
+
     return 0;
 }
