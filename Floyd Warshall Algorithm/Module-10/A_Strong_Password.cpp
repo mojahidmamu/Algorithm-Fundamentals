128d2750e722612275e8f69556502db09cc0700f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-01 23:26:14 +0600

    added

--- Floyd Warshall Algorithm/Module-10/A_Strong_Password.cpp
+++ Floyd Warshall Algorithm/Module-10/A_Strong_Password.cpp
@@ -1,13 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int calculate_time(string s)
+{
+    int time = 2;
+    for (int i = 1; i < s.size(); i++)
+    {
+        if (s[i] == s[i - 1])
+        {
+            time += 1;
+        }
+        else
+        {
+            time += 2;
+        }
+        return time;
+    }
+}
 int main()
 {
     int TestCase;
     cin >> TestCase;
+    cin.ignore();
     while (TestCase--)
     {
         string s;
         cin >> s;
+
+        int best_time = -1;
+        string best_string;
     }
 
     return 0;
