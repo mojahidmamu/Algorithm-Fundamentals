7055d61d16b154da4ba642b2816d3fcfccdf8680
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-01 21:02:32 +0600

    added

--- Floyd Warshall Algorithm/Module-10/Floyd_Warshall_Implementation.cpp
+++ Floyd Warshall Algorithm/Module-10/Floyd_Warshall_Implementation.cpp
@@ -5,12 +5,43 @@ int main()
     int n, e;
     cin >> n >> e;
     int adj_matrix[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                adj_matrix[i][j] = 0;
+            }
+            else
+            {
+                adj_matrix[i][j] = INT_MAX;
+            }
+        }
+    }
+
     while (e--)
     {
         int a, b, c;
         cin >> a >> b >> c;
-        
+        adj_matrix[a][b] = c; // for directed graph:
     }
-    
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (adj_matrix[i][j] == INT_MAX)
+            {
+                cout << "INF" << " ";
+            }
+            else
+            {
+                cout << adj_matrix[i][j] << " ";
+            }
+        }
+        cout << endl;
+    }
     return 0;
 }
