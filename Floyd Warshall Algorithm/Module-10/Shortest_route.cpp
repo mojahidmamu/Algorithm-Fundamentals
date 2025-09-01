0fa630f6feaf21c6c7a1573dbffee984e7ad11ce
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-01 09:26:31 +0600

    added

--- Floyd Warshall Algorithm/Module-10/Shortest_route.cpp
+++ Floyd Warshall Algorithm/Module-10/Shortest_route.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
+    int n, e, Query;
+    cin >> n >> e >> Query;
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+    }
+
+    while (Query--)
+    {
+        int src, dst;
+        cin >> src >> dst;
+    }
+    
     
     return 0;
 }
