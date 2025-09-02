e9707140988f5bc6574d47e321c3654cfde36bd6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-02 08:52:38 +0600

    added

---
+++ Floyd Warshall Algorithm/Module-10/Detect negative weighted cycle using Floyd-Warshall.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    int adj_matrix[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                adj_matrix[i][j] = 0;
+            }
+            else
+            {
+                adj_matrix[i][j] = INT_MAX;
+            }
+        }
+    }
+
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj_matrix[a][b] = c; // for directed graph:
+    }
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (adj_matrix[i][k] != INT_MAX && adj_matrix[k][j] != INT_MAX)
+                {
+                    if (adj_matrix[i][k] + adj_matrix[k][j] < adj_matrix[i][j])
+                    {
+                        adj_matrix[i][j] = adj_matrix[i][k] + adj_matrix[k][j];
+                    }
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (adj_matrix[i][j] == INT_MAX)
+            {
+                cout << "INF" << " ";
+            }
+            else
+            {
+                cout << adj_matrix[i][j] << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
