8f90c83b6177ec13b3d2c19f12b01f81e69a95ca
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-02 22:16:14 +0600

    added

--- Floyd Warshall Algorithm/Module-10/Good Subsequence.cpp
+++ Floyd Warshall Algorithm/Module-10/Good Subsequence.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int main()
 {
-    
+    int TestCase;
+    cin >> TestCase;
+    while (TestCase--)
+    {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 1; i <= N; i++)
+        {
+            cin >> arr[i];
+        }
+        
+    }
+
     return 0;
 }
