6bc22871d0e23f0044af804df1a332ec3fc59c47
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-03 22:06:12 +0600

    added

--- Disjoint_Set_Union_(DSU)/Module-11/Union_Operation_Implementation.cpp
+++ Disjoint_Set_Union_(DSU)/Module-11/Union_Operation_Implementation.cpp
@@ -5,25 +5,27 @@ const int N = 1e5 + 5;
 int par[N];
 int groupSize[N];
 
-// DSU-(find):  
+// DSU-(find): Time Complexity= O(N)
 int DSU_find(int node)
 {
     if (par[node] == -1)
     {
         return node;
     }
-    return DSU_find(par[node]);
+    int leader = DSU_find(par[node]);
+    par[node] = leader;
+    return leader;
 }
 
 // DSU-(union): Time Complexity= O(N)
 int DSU_union(int node1, int node2)
 {
-     int leaderA = DSU_find(node1);
-     int leaderB = DSU_find(node2);
-     par[leaderA] = leaderB;
+    int leaderA = DSU_find(node1);
+    int leaderB = DSU_find(node2);
+    par[leaderA] = leaderB;
 }
 int main()
 {
-    
+
     return 0;
 }
