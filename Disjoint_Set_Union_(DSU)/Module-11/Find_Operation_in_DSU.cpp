5de046b81ab88d62d7c0eec66f725b6bdc2b114b
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-03 20:32:34 +0600

    added

--- Disjoint_Set_Union_(DSU)/Module-11/Find_Operation_in_DSU.cpp
+++ Disjoint_Set_Union_(DSU)/Module-11/Find_Operation_in_DSU.cpp
@@ -1,7 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// DSU:
+int DSU_find(int node)
+{
+    if (par[node] == -1)
+    {
+        return node;
+    }
+    return DSU_find(par[node]);
+}
+
 int main()
 {
-    
+
     return 0;
 }
