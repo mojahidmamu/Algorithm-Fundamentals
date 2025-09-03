6b54d5276fd9d4877d4515539d784759c753d3d6
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-03 22:19:55 +0600

    added

--- Disjoint_Set_Union_(DSU)/Module-11/Union_Operation_Implementation.cpp
+++ Disjoint_Set_Union_(DSU)/Module-11/Union_Operation_Implementation.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 const int N = 1e5 + 5;
-int par[N];
+int parent[N];
 int groupSize[N];
 
 // DSU-(find): Time Complexity= O(logN)
 int DSU_find(int node)
 {
-    if (par[node] == -1)
+    if (parent[node] == -1)
     {
         return node;
     }
-    int leader = DSU_find(par[node]);
-    par[node] = leader;
+    int leader = DSU_find(parent[node]);
+    parent[node] = leader;
     return leader;
 }
 
@@ -22,17 +22,28 @@ int DSU_union(int node1, int node2)
 {
     int leaderA = DSU_find(node1);
     int leaderB = DSU_find(node2);
-    par[leaderA] = leaderB;
+    // par[leaderA] = leaderB;
+    if (groupSize[leaderA] > groupSize[leaderB])
+    {
+        parent[leaderB] = leaderA;
+        groupSize[leaderA] += groupSize[leaderB];
+    }
+    else
+    {
+        parent[leaderA] = leaderB;
+        groupSize[leaderB] += groupSize[leaderA];
+    }
 }
 
 int main()
 {
-    memset(par, -1, sizeof(par));
+    memset(parent, -1, sizeof(parent));
     memset(groupSize, 1, sizeof(groupSize));
-    
-    DSU_union(1, 2);
 
-    int result = DSU_find(4);
-    cout << result << endl;
+    DSU_union(1, 2);
+    DSU_union(2, 3);
+
+    cout << DSU_find(3) << endl;
+    cout << DSU_find(4) << endl;
     return 0;
 }
