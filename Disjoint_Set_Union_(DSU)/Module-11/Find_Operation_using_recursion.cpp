0218f14e7650b6edb679b75441bb86ca1017f217
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-03 21:33:59 +0600

    added

---
+++ Disjoint_Set_Union_(DSU)/Module-11/Find_Operation_using_recursion.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+int par[N];
+int groupSize[N];
+
+// DSU-(find): Time Complexity= O(N)
+int DSU_find(int node)
+{
+    if (par[node] == -1)
+    {
+        node = par[node];
+    }
+    return node;
+    // return DSU_find(par[node]);
+}
+
+int main()
+{
+    memset(par, -1, sizeof(par));
+    par[0] = 1;
+    par[1] = 1;
+    par[2] = 1;
+    par[3] = 1;
+    par[4] = 5;
+    par[5] = 3;
+
+    cout << par[4] << endl;
+    return 0;
+}
