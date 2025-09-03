caec43385d4a20efa00c34d4bec4cdef4064fd62
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-03 21:55:09 +0600

    added

---
+++ Disjoint_Set_Union_(DSU)/Module-11/Find_Operation_optimized.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+int par[N];
+int groupSize[N];
+
+// DSU-(find): Time Complexity= O(N)
+int DSU_find(int node)
+{
+    if (par[node] == -1)
+    {
+        return node;
+    }
+    int leader = DSU_find(par[node]);
+    par[node] = leader;
+    return leader;
+}
+
+int main()
+{
+    memset(par, -1, sizeof(par));
+    par[0] = 1;
+    par[1] = 1;
+    par[2] = 1;
+    par[3] = 1;
+    par[4] = 5;
+    par[5] = 3;
+
+    int result = DSU_find(4);
+    cout << result << endl;
+    return 0;
+}
