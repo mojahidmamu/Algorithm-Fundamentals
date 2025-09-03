7f43d6dcee0efd36bd6c8da74c76796c3be88711
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-03 20:38:36 +0600

    added

--- Disjoint_Set_Union_(DSU)/Module-11/Union_Operation_Implementation.cpp
+++ Disjoint_Set_Union_(DSU)/Module-11/Union_Operation_Implementation.cpp
@@ -1,16 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// DSU-(union):  
-int DSU_union(int node)
+// DSU-(find):  
+int DSU_find(int node)
 {
     if (par[node] == -1)
     {
         return node;
     }
-    return DSU_union(par[node]);
+    return DSU_find(par[node]);
 }
 
+// DSU-(union): Time Complexity= O(N)
+int DSU_union(int node1, int node2)
+{
+     int leaderA = DSU_find(node1);
+     int leaderB = DSU_find(node2);
+     par[leaderA] = leaderB;
+}
 int main()
 {
     
