208c8d018922a88b9e8d3a519f71710419fe9d36
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-05 11:54:39 +0600

    added

--- Assignment-2/Module-12/Shortest_distance.cpp
+++ Assignment-2/Module-12/Shortest_distance.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 int main()
 {
-    
+    int N, E;
+    cin >> N >> E;
+    while (E--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+    }
+
+    int TestCase;
+    cin >> TestCase;
+    while (TestCase--)
+    {
+        int src, dst;
+        cin >> src >> dst;
+    }
     return 0;
 }
