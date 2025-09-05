d1598b0e944750ed3a7418c6ce0b4e0e83608569
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-05 11:52:52 +0600

    added

--- Assignment-2/Module-12/Can_go_again.cpp
+++ Assignment-2/Module-12/Can_go_again.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 int main()
 {
-    
+    int N, E;
+    cin >> N >> E;
+    while (E--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+    }
+    int source;
+    cin >> source;
+
+    int TestCase;
+    cin >> TestCase;
+    while (TestCase--)
+    {
+        int dst;
+        cin >> dst;
+    }
+
     return 0;
 }
