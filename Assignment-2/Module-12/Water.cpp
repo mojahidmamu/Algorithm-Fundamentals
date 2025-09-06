e13b4693b4e02c6a9143d303fbd8c696f8d03dbd
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-06 13:44:49 +0600

    added

--- Assignment-2/Module-12/Water.cpp
+++ Assignment-2/Module-12/Water.cpp
@@ -1,10 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+pair<int, int> maxWaterContainer(vector<int> &arr)
+{
+    int left = 0;
+    int right = arr.size() - 1;
+    int maxHeight = 0;
+    pair<int, int> bestPair = {0, 0};
+
+    while (left < right)
+    {
+        int height = min(arr[left], arr[right]);
+        if (height > maxHeight)
+        {
+            maxHeight = height;
+            bestPair = {left, right};
+        }
+
+        if (arr[left] < arr[right])
+        {
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+    }
+    return bestPair;
+}
+
 int main()
 {
-    int TestCase;
-    cin >> TestCase;
-    while (TestCase--)
+    int T;
+    cin >> T;
+    while (T--)
     {
         int N;
         cin >> N;
@@ -14,34 +43,8 @@ int main()
             cin >> arr[i];
         }
 
-        int left = 0;
-        int right =N - 1;
-        int maxArea = 0;
-        pair<int, int> bestPair = {0, 0}; // best-pair;
-
-        while (left < right)
-        {
-            int h = min(arr[left], arr[right]);
-            int w = right - left;
-            int area = h * w;
-
-            if (area > maxArea)
-            {
-                maxArea = area;
-                bestPair = {left, right};
-            }
-
-            if (arr[left] < arr[right])
-            {
-                left++;
-            }
-            else
-            {
-                right--;
-            }
-        }
-        cout << bestPair.first << " " << bestPair.second << endl;
+        auto ans = maxWaterContainer(arr);
+        cout << ans.first << " " << ans.second << endl;
     }
-
     return 0;
 }
