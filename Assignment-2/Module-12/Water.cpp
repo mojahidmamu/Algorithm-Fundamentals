8b5c4467afb21e9b3da8d3a4c384c0bcbd5b8d20
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-06 12:44:41 +0600

    added

--- Assignment-2/Module-12/Water.cpp
+++ Assignment-2/Module-12/Water.cpp
@@ -8,10 +8,22 @@ int main()
     {
         int N;
         cin >> N;
-        int arr[N];
+        vector<int> arr(N);
         for (int i = 0; i < N; i++)
         {
             cin >> arr[i];
+        }
+
+        int left = 0;
+        int right = arr.size() - 1;
+        int maxArea = 0;
+        pair<int, int> bestPair = {0, 0}; // best-pair;
+
+        while (left < right)
+        {
+            int h = 
+            int w = right - left;
+            int area = h * w;
         }
         
     }
