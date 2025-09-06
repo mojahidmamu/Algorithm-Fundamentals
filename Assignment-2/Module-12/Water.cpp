b8d75c2a8d6edd3848ea7282394441cef11c97df
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-06 12:34:55 +0600

    added

--- Assignment-2/Module-12/Water.cpp
+++ Assignment-2/Module-12/Water.cpp
@@ -6,7 +6,14 @@ int main()
     cin >> TestCase;
     while (TestCase--)
     {
-        /* code */
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++)
+        {
+            cin >> arr[i];
+        }
+        
     }
 
     return 0;
