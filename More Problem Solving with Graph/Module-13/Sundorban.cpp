6b654eba8987f0230080989af623d2849a034b93
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-07 16:56:10 +0600

    added

--- More Problem Solving with Graph/Module-13/Sundorban.cpp
+++ More Problem Solving with Graph/Module-13/Sundorban.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 int main()
 {
-    
+    int N;
+    cin >> N;
+    char arr[N][N];
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+     
+
     return 0;
 }
