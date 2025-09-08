209cd2d60d01af9b5b5daa8df5242189dba8c1ca
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-08 20:13:50 +0600

    added

--- Basic_of_Dynamic_Programming/Module-14/Naive_recursion.cpp
+++ Basic_of_Dynamic_Programming/Module-14/Naive_recursion.cpp
@@ -1,13 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fibonacci()
+int fibonacci(int n)
 {
-
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
 int main()
 {
-    
+    cout << fibonacci(10) << endl;
     return 0;
 }
