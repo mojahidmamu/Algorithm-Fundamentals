12bb94b460e3002555d2f3e94d0c5e415eb84e33
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-08 20:15:23 +0600

    added

--- Basic_of_Dynamic_Programming/Module-14/Top_Down.cpp
+++ Basic_of_Dynamic_Programming/Module-14/Top_Down.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int fibonacci(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n - 2); // same code reuse: 
+    }
+}
+
 int main()
 {
-    
+    cout << fibonacci(10) << endl;
     return 0;
 }
