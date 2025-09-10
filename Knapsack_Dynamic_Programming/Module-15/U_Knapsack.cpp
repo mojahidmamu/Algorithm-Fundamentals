90f2c6704f94cc3d876ea065d4032c0a26074062
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-10 11:44:05 +0600

    added

--- Knapsack_Dynamic_Programming/Module-15/U_Knapsack.cpp
+++ Knapsack_Dynamic_Programming/Module-15/U_Knapsack.cpp
@@ -5,13 +5,39 @@ int N, W;
 int weight[25], value[25];
 int dp[25][105];
 
+int Knapsack(int i, int w)
+{
+    if (N == 0 || w == 0)
+    {
+        return 0;
+    }
+
+    if (dp[i][w] != -1)
+    {
+        return dp[i][w];
+    }
+
+    // opt-1:
+    int ans = Knapsack(i + 1, w);
+    // opt-2:
+    if (weight[i] <= w)
+    {
+        ans = max(ans, value[i] + Knapsack(i + 1, w - weight[i]));
+    }
+    
+    return dp[i][w] = ans;
+}
+
 int main()
 {
     cin >> N >> W;
-    for(int i = 0; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
         cin >> weight[i] >> value[i];
     }
+
+    memset(dp, -1, sizeof(dp));
+    cout << Knapsack(0, W);
 
     return 0;
 }
