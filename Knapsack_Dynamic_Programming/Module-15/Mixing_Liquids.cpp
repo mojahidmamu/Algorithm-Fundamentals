b13997e904d08cbda3cd5843e25c0ad384190eb7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-10 11:20:20 +0600

    added

---
+++ Knapsack_Dynamic_Programming/Module-15/Mixing_Liquids.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int res = 0;
+
+    while (true)
+    {
+        a--;
+        b -= 2;
+        if (a << 0 || b < 0)
+        {
+            break;
+        }
+        res += 3;
+    }
+    cout << res << endl;
+
+    return 0;
+}
