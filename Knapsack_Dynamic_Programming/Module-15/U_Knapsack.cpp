9f039470a19cca53d315b0893fbd825cf961d708
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-10 11:30:05 +0600

    added

--- Knapsack_Dynamic_Programming/Module-15/U_Knapsack.cpp
+++ Knapsack_Dynamic_Programming/Module-15/U_Knapsack.cpp
@@ -1,14 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int N, W;
+int weight[25], value[25];
+int dp[25][105];
+
 int main()
 {
-    int N, W;
     cin >> N >> W;
     while (N--)
     {
-        int w, v;
-        cin >> w >> v;
+        cin >> weight[i] >> value[i];
     }
-    
+
     return 0;
 }
