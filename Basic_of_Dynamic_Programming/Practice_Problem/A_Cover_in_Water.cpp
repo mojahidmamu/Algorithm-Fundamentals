977c14230dfdc6083e16eb03ef90b352b96beb19
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-11 09:49:27 +0600

    added

--- Basic_of_Dynamic_Programming/Practice_Problem/A_Cover_in_Water.cpp
+++ Basic_of_Dynamic_Programming/Practice_Problem/A_Cover_in_Water.cpp
@@ -3,10 +3,30 @@ using namespace std;
 int main()
 {
     int TestCase;
-    cin >>  TestCase;
-    while(TestCase--)
+    cin >> TestCase;
+    while (TestCase--)
     {
-        
+        int N;
+        cin >> N;
+        string s;
+        cin >> s;
+
+        int consecutive = 0;
+        for (char ch : s)
+        {
+            if (ch == '.')
+            {
+                consecutive++;
+            }
+        }
+        if (consecutive == 3)
+        {
+            cout << 2 << endl;
+        }
+        else
+        {
+            cout <<
+        }
     }
     return 0;
 }
