a01c1a50b6be23e1dd270f7d3f14da09f70e5f49
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-11 11:06:32 +0600

    added

--- Basic_of_Dynamic_Programming/Practice_Problem/B_Print_from_1_to_N.cpp
+++ Basic_of_Dynamic_Programming/Practice_Problem/B_Print_from_1_to_N.cpp
@@ -1,7 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+void recursion(int N)
+{
+    if (N == 0)
+    {
+        return;
+    }
+    recursion(N - 1);
+    cout << N << endl;
+}
 int main()
 {
-    
+    int N;
+    cin >> N;
+    recursion(N);
     return 0;
 }
