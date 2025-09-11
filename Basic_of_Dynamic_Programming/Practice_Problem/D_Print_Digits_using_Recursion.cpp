d094eb22c6b75cc72c969d7a9b9ac629530789ef
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-11 10:33:26 +0600

    added

--- Basic_of_Dynamic_Programming/Practice_Problem/D_Print_Digits_using_Recursion.cpp
+++ Basic_of_Dynamic_Programming/Practice_Problem/D_Print_Digits_using_Recursion.cpp
@@ -1,7 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+void printDigit(int N)
+{
+    if (N < 10)
+    {
+        cout << N;
+        return;
+    }
+    else
+    {
+        printDigit(N / 10);
+        cout << " " << N % 10;
+    }
+}
+
 int main()
 {
-    
+    int TestCase;
+    cin >> TestCase;
+    while (TestCase--)
+    {
+        int N;
+        cin >> N;
+        printDigit(N);
+    }
+
     return 0;
 }
