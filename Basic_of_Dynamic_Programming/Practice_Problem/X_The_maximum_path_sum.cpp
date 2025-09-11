76ee0cf4632fe7b71ea33131f83dfdf3bfbc5e6c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-11 11:48:53 +0600

    added

--- Basic_of_Dynamic_Programming/Practice_Problem/X_The_maximum_path_sum.cpp
+++ Basic_of_Dynamic_Programming/Practice_Problem/X_The_maximum_path_sum.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
-    
+    int N, M;
+    cin >> N >> M;
+    int arr[N][M];
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
     return 0;
 }
