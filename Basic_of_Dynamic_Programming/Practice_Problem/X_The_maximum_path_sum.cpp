402b81d6c4dc850197033a4a92b75c5083c4bd69
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-11 12:21:26 +0600

    added

--- Basic_of_Dynamic_Programming/Practice_Problem/X_The_maximum_path_sum.cpp
+++ Basic_of_Dynamic_Programming/Practice_Problem/X_The_maximum_path_sum.cpp
@@ -1,10 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+int N, M;
+int arr[10][10];
+
+int maxSum(int i, int j)
+{
+    if (i == N - 1 && j == M - 1)
+    {
+        return arr[i][j];
+    }
+
+    int down = INT_MIN, right = INT_MIN;
+
+    if (i + 1 < N)
+    {
+        down = maxSum(i + 1, j);
+    }
+
+    if (j + 1 < M)
+    {
+        right = maxSum(i, j + 1);
+    }
+    return arr[i][j] + max(down, right);
+}
+
 int main()
 {
-    int N, M;
     cin >> N >> M;
-    int arr[N][M];
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < M; j++)
@@ -13,5 +35,6 @@ int main()
         }
     }
 
+    cout << maxSum(0, 0) << endl;
     return 0;
 }
