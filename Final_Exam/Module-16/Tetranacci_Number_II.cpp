172683d5f1e8a7f8587960692d10dc4f1452e2d0
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-12 12:33:13 +0600

    added

--- Final_Exam/Module-16/Tetranacci_Number_II.cpp
+++ Final_Exam/Module-16/Tetranacci_Number_II.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
-    
+    int n;
+    cin >> n;
+    int fibo[1005];
+    fibo[0] = 0;
+    fibo[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    cout << fibo[n] << endl;
     return 0;
 }
