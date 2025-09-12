7f25362e907f2ffb310e54ca67c6a84f6ed5846e
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-12 12:45:56 +0600

    added

--- Final_Exam/Module-16/Adventure.cpp
+++ Final_Exam/Module-16/Adventure.cpp
@@ -1,5 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int N, W;
+int weight[25], value[25];
+int dp[25][105];
+
+int Knapsack(int i, int w)
+{
+    if (i == N)
+    {
+        return 0;
+    }
+    if (w == 0)
+    {
+        return 0;
+    }
+
+    if (dp[i][w] != -1)
+    {
+        return dp[i][w];
+    }
+
+    // opt-1:
+    int ans = Knapsack(i + 1, w);
+    // opt-2:
+    if (weight[i] <= w)
+    {
+        ans = max(ans, value[i] + Knapsack(i + 1, w - weight[i]));
+    }
+
+    return dp[i][w] = ans;
+}
+
 int main()
 {
     int T;
@@ -13,12 +45,15 @@ int main()
         {
             cin >> weight_arr[i];
         }
-        
+
         int value_arr[N];
         for (int i = 0; i < N; i++)
         {
             cin >> value_arr[i];
         }
+
+        memset(dp, -1, sizeof(dp));
+        cout << Knapsack(0, W);
     }
 
     return 0;
