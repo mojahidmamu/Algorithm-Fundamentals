4b356835815328df131b78ff07b031d864536f6f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-12 15:40:10 +0600

    added

--- Final_Exam/Module-16/Pile_of_word.cpp
+++ Final_Exam/Module-16/Pile_of_word.cpp
@@ -1,5 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+bool checkEqual(string s1, string s2)
+{
+    if (s1 == s2)
+    {
+        return true;
+    }
+
+
+}
+
 int main()
 {
     int TestCase;
@@ -7,7 +18,17 @@ int main()
     while (TestCase--)
     {
         string s1, s2;
+        cin >> s1 >> s2;
+
+        if (checkEqual(s1, s2) == true)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
-    
+
     return 0;
 }
