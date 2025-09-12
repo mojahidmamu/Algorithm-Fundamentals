cf5e68eb0ccad9b5708e4f91720890cd02f50891
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-12 12:36:35 +0600

    added

--- Final_Exam/Module-16/Tetranacci_Number_II.cpp
+++ Final_Exam/Module-16/Tetranacci_Number_II.cpp
@@ -7,10 +7,12 @@ int main()
     int fibo[1005];
     fibo[0] = 0;
     fibo[1] = 1;
+    fibo[2] = 1;
+    fibo[3] = 2;
 
-    for (int i = 2; i <= n; i++)
+    for (int i = 4; i <= n; i++)
     {
-        fibo[i] = fibo[i - 1] + fibo[i - 2];
+        fibo[i] = fibo[i - 1] + fibo[i - 2] + fibo[i - 3] + fibo[i - 4];
     }
     cout << fibo[n] << endl;
     return 0;
