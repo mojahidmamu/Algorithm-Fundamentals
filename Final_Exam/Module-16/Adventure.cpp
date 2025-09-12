b82744446d5d8288098fe07001582160d1bd0619
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-12 12:40:21 +0600

    added

--- Final_Exam/Module-16/Adventure.cpp
+++ Final_Exam/Module-16/Adventure.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 int main()
 {
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, W;
+        cin >> N >> W;
+        int weight_arr[W];
+        for (int i = 0; i < W; i++)
+        {
+            cin >> weight_arr[i];
+        }
+    }
+
     return 0;
 }
