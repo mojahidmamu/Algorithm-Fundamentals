83ea0d6ff317f18527f7c4af75211f85ed13bb7f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-12 11:04:59 +0600

    added

--- Final_Exam/Module-16/Make_it.cpp
+++ Final_Exam/Module-16/Make_it.cpp
@@ -1,7 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// bool canReach(long long int current)
+// {
+//     if (current == N)
+//     {
+//         return true;
+//     }
+//     if (current > N)
+//     {
+//         return false;
+//     }
+//     // bool l = canReach(current + 3);
+//     // bool r = canReach(current * 2);
+//     // return l || r;
+//     return canReach(current + 3) || canReach(current * 2);
+// }
 int main()
 {
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        long long int N;
+        cin >> N;
+        if (N % 4 == 3)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+
+        // if (canReach(1))
+        // {
+        //     cout << "YES" << endl;
+        // }
+        // else
+        // {
+        //     cout << "NO" << endl;
+        // }
+    }
+
     return 0;
 }
+//
