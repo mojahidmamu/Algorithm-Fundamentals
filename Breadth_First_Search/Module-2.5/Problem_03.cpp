85bc68126c64c5218ce683d9c72d5463104930dc
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-19 12:31:14 +0600

    added

--- Breadth_First_Search/Module-2.5/Problem_03.cpp
+++ Breadth_First_Search/Module-2.5/Problem_03.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
+    int N, E;
+    cin >> N >> E;
+    vector<int> adj_list[N];
+
+    while (E--)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    int node;
+    cin >> node;
+
+    cout << adj_list[node].size() << endl;
     
+
     return 0;
 }
