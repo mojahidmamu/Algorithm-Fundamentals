685e132bcec2f59f41419f1204321b389c276da4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-19 11:20:09 +0600

    added

--- Breadth_First_Search/Module-2.5/Problem_01.cpp
+++ Breadth_First_Search/Module-2.5/Problem_01.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 int main()
 {
+    int N, E;
+    cin >> N >> E;
+    vector<int> path[N];
+    while (E--)
+    {
+        int a, b;
+        cin >> a >> b;
+        path[a].push_back(b);
+        path[b].push_back(a);
+    }
+
+    int Q;
+    cin >> Q;
+    while (Q--)
+    {
+        int src, dst;
+        cin >> src >> dst;
+        
+    }
+    
     
     return 0;
 }
