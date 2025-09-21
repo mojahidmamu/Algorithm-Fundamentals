d1f76cf62ec50db906416ce3a02c2f995c949f98
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-21 10:22:08 +0600

    added

--- Dijkstra_Algorithm/Module-7/Practice.cpp
+++ Dijkstra_Algorithm/Module-7/Practice.cpp
@@ -1,10 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 1e9;
+vector<pair<int, int>> adj_list[1005]; // array of vector with every index have a pair(int, int)
+int dis[1005];
+
+void dijkstra(int source)
+{
+    queue<pair<int, int>> q;
+    q.push({source, 0});
+    dis[source] = 0;
+    while (!q.empty())
+    {
+        auto par = q.front(); // auto for access one pair<int, int>
+        q.pop();
+        int par_node = par.first;
+        int par_dis = par.second;
+
+        for (auto child : adj_list[par_node])
+        {
+            int child_node = child.first;
+            int child_dis = child.second;
+
+            if (par_dis + child_dis < dis[child_node])
+            {
+                dis[child_node] = par_dis + child_dis;
+                q.push({child_node, dis[child_node]});
+            }
+        }
+    }
+}
 
 int main()
 {
+    int N, E;
+    cin >> N >> E;
+    while (E--)
+    {
+        int a, b, c;
+        cin >> a >> c;
+        adj_list[a].push_back({b, c});
+        adj_list[b].push_back({c, c});
+    }
+
+    
     
     return 0;
 }
