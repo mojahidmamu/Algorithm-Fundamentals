fa0566eccc75dc83b44ef59d01c7e809363a8988
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-22 10:38:46 +0600

    added

--- More Problem Solving with Graph/Module-13/D_Roads_not_only_in_Berland.cpp
+++ More Problem Solving with Graph/Module-13/D_Roads_not_only_in_Berland.cpp
@@ -1,7 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+const int N = 1e5 + 5;
+int parent[N];
+int group_size[N];
+
+
+// DSU-(find): Time Complexity= O(logN)
+int DSU_find(int node)
+{
+    if (par[node] == -1)
+    {
+        return node;
+    }
+    int leader = DSU_find(par[node]);
+    par[node] = leader;
+    return leader;
+}
+
 int main()
 {
-    
+
     return 0;
 }
