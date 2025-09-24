72da4141a1006d6db294227f6bc178886c9e4dd8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-24 12:36:39 +0600

    added

--- Final_Exam/Module-16/Pile_of_word.cpp
+++ Final_Exam/Module-16/Pile_of_word.cpp
@@ -1,20 +1,46 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// bool checkEqual(string s1, string s2)
+// {
+//     if (s1.size() != s2.size())
+//     {
+//         return false;
+//     }
+//     sort(s1.begin(), s1.end());
+//     sort(s2.begin(), s2.end());
+//     if (s1 == s2)
+//     {
+//         return true;
+//     }
+//     return false;
+// }
+
+// int main()
+// {
+//     int TestCase;
+//     cin >> TestCase;
+//     while (TestCase--)
+//     {
+//         string s1, s2;
+//         cin >> s1 >> s2;
+
+//         if (checkEqual(s1, s2))
+//         {
+//             cout << "YES" << endl;
+//         }
+//         else
+//         {
+//             cout << "NO" << endl;
+//         }
+//     }
+
+//     return 0;
+// }
+// // Accepted:
+
 #include <bits/stdc++.h>
 using namespace std;
-
-bool checkEqual(string s1, string s2)
-{
-    if (s1.size() != s2.size())
-    {
-        return false;
-    }
-    sort(s1.begin(), s1.end());
-    sort(s2.begin(), s2.end());
-    if (s1 == s2)
-    {
-        return true;
-    }
-    return false;
-}
 
 int main()
 {
@@ -24,17 +50,21 @@ int main()
     {
         string s1, s2;
         cin >> s1 >> s2;
-
-        if (checkEqual(s1, s2))
+        if (s1.size() != s2.size())
+        {
+            cout << "NO" << endl;
+        }
+        sort(s1.begin(), s1.end());
+        sort(s2.begin(), s2.end());
+        if (s1 == s2)
         {
             cout << "YES" << endl;
         }
-        else
-        {
-            cout << "NO" << endl;
+        else {
+             cout << "NO" << endl;
         }
     }
 
     return 0;
 }
-// Accepted: 
+// Accepted:
