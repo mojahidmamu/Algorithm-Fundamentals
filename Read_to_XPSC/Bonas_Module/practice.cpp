2dfbb3b6d1e7f0a294f88de608a7d2f9e364ddb4
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-26 19:48:53 +0600

    added

--- Read_to_XPSC/Bonas_Module/practice.cpp
+++ Read_to_XPSC/Bonas_Module/practice.cpp
@@ -5,6 +5,21 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    vector<int> v;
+    v.push_back(5);
+    v.push_back(10);
+    v.push_back(20);
+
+    sort(v.begin(), v.end());                         // sort
+    reverse(v.begin(), v.end());                      // reverse order
+    cout << *max_element(v.begin(), v.end()) << endl; // max
+    cout << *min_element(v.begin(), v.end()) << endl; // min
+    // sort(v.rbegin(), rend());
+    // reverse(v.begin(), v.end());
+    // *max_element(v.begin(), v.end());
+    // *min_element(v.begin(), v.end());
+    // binary_search(v.begin(), v.end(), x); // findout x form sorted array
+    cout << "total sum of " <<  accumulate(v.begin(), v.end(), 0) << endl;
+
     return 0;
 }
