34696e8dd80170d69059b690290565e80c119065
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-26 17:12:11 +0600

    added

--- Read_to_XPSC/Bonas_Module/A_Doremy_s_Paint_3.cpp
+++ Read_to_XPSC/Bonas_Module/A_Doremy_s_Paint_3.cpp
@@ -1,9 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        
+    }
+
     return 0;
 }
