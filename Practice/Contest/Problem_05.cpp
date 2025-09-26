ecf1a33bf94759e035bf7a1894088062327b8a14
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-26 23:05:58 +0600

    added

--- Practice/Contest/Problem_05.cpp
+++ Practice/Contest/Problem_05.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, Q;
-vector<int> adj_list[1005];
-vector<int> indegree; // track parents
-vector<int> strength;
-set<pair<int, int>> cuts;
+// int N, Q;
+// vector<int> adj_list[1005];
+// vector<int> indegree; // track parents
+// vector<int> strength;
 
 // bool vis[1005];
 // vector<int> adj_list[1005];
@@ -39,40 +38,74 @@ int main()
     cin.tie(nullptr);
     // --- your code here ---
 
-    cin >> N;
-    indegree.assign(N + 1, 0);
-    strength.assign(N + 1, 0);
+    int N;
+    if (!(cin >> N))
+    {
+        return 0;
+    }
+
+    vector<vector<int>> adj_list(N + 1);
+    vector<int> indeg(N + 1, 0);
 
     for (int i = 0; i < N - 1; i++)
     {
         int a, b;
         cin >> a >> b;
         adj_list[a].push_back(b);
-        indegree[b]++;
+        indeg[b]++;
     }
 
+    int Q;
     cin >> Q;
+    set<pair<int, int>> cuts;
     while (Q--)
     {
         int U, V;
         cin >> U >> V;
         cuts.insert({U, V});
-        indegree[V]--;
+        indeg[V]--;
     }
 
-    for (int i = 1; i <= N; i++)
+    vector<long long> strength(N + 1, 0);
+    vector<char> vis(N + 1, 0);
+
+    for (int i = 1; i <= N; ++i)
     {
-        if (indegree[i] == 0)
+        if (indeg[i] == 0 && !vis[i])
         {
-            dfs(i, 0);
+            // BFS from root i
+            queue<pair<int, int>> q; // (node, depth)
+            q.push({i, 0});
+            vis[i] = 1;
+
+            while (!q.empty())
+            {
+                auto [u, d] = q.front();
+                q.pop();
+
+                if (d == 0)
+                    strength[u] = 0;
+                else if (d < 62)
+                    strength[u] = (1LL << d); // safe shift
+                else
+                    strength[u] = (1LL << 62); // clamp large depth to avoid UB
+
+                for (int v : adj[u])
+                {
+                    if (cuts.count({u, v}))
+                        continue; // edge removed
+                    if (vis[v])
+                        continue;
+                    vis[v] = 1;
+                    q.push({v, d + 1});
+                }
+            }
         }
     }
 
-    for (int i = 1; i <= N; i++)
+    for (int i = 1; i <= N; ++i)
     {
-        cout << strength[i] << " ";
+        cout << strength[i] << (i == N ? '\n' : ' ');
     }
-    cout << endl;
-
     return 0;
 }
--- Practice/Contest/problem_06.cpp
+++ Practice/Contest/problem_06.cpp
@@ -1,9 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     // --- your code here ---
-    
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+   
+    while (Q--)
+    {
+        int L, R;
+        cin >> L >> R;
+        L--;
+        R--;
+
+        unordered_set<int> st;
+        for (int i = L; i <= R; i++)
+        {
+            st.insert(arr[i]);
+        }
+
+        int mex = 0;
+        while (st.count(mex))
+        {
+            mex++;
+        }
+        cout << mex << "\n";
+    }
+
     return 0;
 }
