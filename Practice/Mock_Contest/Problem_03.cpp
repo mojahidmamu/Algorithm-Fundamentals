1e30e5edcb97a309ef47753e324d6bf828a4b268
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-09-26 10:51:01 +0600

    added

--- Practice/Mock_Contest/Problem_03.cpp
+++ Practice/Mock_Contest/Problem_03.cpp
@@ -1,5 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+long long rangeSUm(long long int n)
+{
+    return ((n * (n + 1)) / 2);
+}
+
+long long SUM(int L, int R)
+{
+    return rangeSUm(R) - rangeSUm(L - 1);
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -11,6 +22,20 @@ int main()
     {
         int L, R;
         cin >> L >> R;
+
+        int x = (L + 1) / 2;
+        int y = (R + 1) / 2;
+        long long ans = (2 * SUM(x, y));
+
+        if (L % 2 == 0)
+        {
+            ans -= x;
+        }
+        if (R % 2 != 0)
+        {
+            ans -= y;
+        }
+        cout << ans << endl;
     }
 
     return 0;
